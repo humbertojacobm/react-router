f83fb9ee60bc00bf22b293048e7ddfb3fea9e0eb
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-02-12 17:30:51 -0500

    modulo 01 video 04 configurando un spa en webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,8 +2,9 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    "home": path.resolve(__dirname, 'src/entries/home.js'),
-    "redux": path.resolve(__dirname, 'src/entries/redux.js'),
+    "app": path.resolve(__dirname, 'src/entries/app.js'),
+    // "videos": path.resolve(__dirname, 'src/entries/videos.js'),
+    // "redux": path.resolve(__dirname, 'src/entries/redux.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
