7037861ad77fc734e727a015933136d323cf425b
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-02-13 18:39:23 -0500

    modulo 02 video 07 link y navlink

--- src/entries/app.js
+++ src/entries/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 import { Provider } from 'react-redux';
@@ -9,6 +9,8 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { BrowserRouter } from 'react-router-dom';
+import Header from '../pages/components/header';
+
 // function logger({ getState, dispatch}) {
 //   return (next) => {
 //     return (action) => {
@@ -46,11 +48,13 @@ const homeContainer = document.getElementById('home-container')
 
 
 render(
-  <BrowserRouter
-    basename="/videos"
+  <BrowserRouter    
   >
     <Provider store={store}>
-      <Home />
+      <Fragment>
+        <Header/>
+        <Home />
+      </Fragment>
     </Provider>
   </BrowserRouter>
 , homeContainer);
